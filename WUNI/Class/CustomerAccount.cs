48560a5bb2acb9c5b59c790cd6236794ca65b368
DOCUTEE <quangforwork1203@gmail.com>
2024-04-10 20:29:03 +0700

    BUILDING A STRONG KNOWLEDGE

---
+++ WUNI/Class/CustomerAccount.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WUNI.Class
+{
+    internal class CustomerAccount
+    {
+    }
+}
---
+++ WUNI/DAOClass/CustomerAccountDAO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WUNI.DAOClass
+{
+    internal class CustomerAccountDAO
+    {
+    }
+}
