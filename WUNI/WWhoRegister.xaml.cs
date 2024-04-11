caa946ee7c2f6ac6caae4e0e82a0d1a8bdb27a18
DOCUTEE <quangforwork1203@gmail.com>
2024-04-11 19:22:44 +0700

    SAP XONG LOGIN

---
+++ WUNI/WWhoRegister.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WUNI
+{
+    /// <summary>
+    /// Interaction logic for WWhoRegister.xaml
+    /// </summary>
+    public partial class WWhoRegister : Window
+    {
+        public WWhoRegister()
+        {
+            InitializeComponent();
+        }
+        private void btnIsWorker_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WRegisterWorker wRegisterWorker = new WRegisterWorker();
+            wRegisterWorker.Show();
+            this.Close();
+        }
+
+        private void btnIsCustomer_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WRegisterCustomer wRegisterCustomer = new WRegisterCustomer();
+            wRegisterCustomer.Show();
+            this.Close();
+        }
+    }
+}
