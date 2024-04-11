0692a704fde603aab379ca1576635821379ed6fa
DOCUTEE <quangforwork1203@gmail.com>
2024-04-12 02:24:36 +0700

    Done Basic Frontend for Login and Register Page

--- WUNI/WINDOWS/WRegisterCustomer.xaml.cs
+++ WUNI/WINDOWS/WRegisterCustomer.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,28 @@ namespace WUNI.WINDOWS
         public WRegisterCustomer()
         {
             InitializeComponent();
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            //Thịnh code ở đây sao cho thông tin và tài khoản của người dùng lần lượt được lưu vào bảng Customer và CustomerAccount
+        }
+
+        private void SelectImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.png;*.jpeg;*.jpg)|*.png;*.jpeg;*.jpg|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                // Tạo một BitmapImage
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(openFileDialog.FileName);
+                bitmap.EndInit();
+
+                // Đặt ảnh cho Image control
+                imgProfile.Source = bitmap;
+            }
         }
     }
 }
--- WUNI/WINDOWS/WRegisterWorker.xaml.cs
+++ WUNI/WINDOWS/WRegisterWorker.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -25,7 +26,26 @@ namespace WUNI.WINDOWS
             InitializeComponent();
 
         }
+        public void SelectImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.png;*.jpeg;*.jpg)|*.png;*.jpeg;*.jpg|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                // Tạo một BitmapImage
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(openFileDialog.FileName);
+                bitmap.EndInit();
 
-      
+                // Đặt ảnh cho Image control
+                imgProfile.Source = bitmap;
+            }
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            //Thịnh viết code bắt đầu từ đây để khi bấm đăng ký thì thông tin thợ và account thợ sẽ lần lượt được add vào bảng Worker và WorkerAccount 
+        }
     }
 }
