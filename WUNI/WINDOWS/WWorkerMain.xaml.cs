f3edd22afc934892793931b29bceb62fc91b25b5
DOCUTEE <quangforwork1203@gmail.com>
2024-04-16 01:36:46 +0700

    Doing Worker Page

--- WUNI/WINDOWS/WWorkerMain.xaml.cs
+++ WUNI/WINDOWS/WWorkerMain.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +23,123 @@ namespace WUNI.WINDOWS
         public WWorkerMain()
         {
             InitializeComponent();
+            string path = Environment.CurrentDirectory;
+            string path1 = Directory.GetParent(path).Parent.Parent.FullName;
+            iconFindWork.Source = new BitmapImage(new Uri(path1 + "\\Logo\\FindIcon.png"));
+            iconHistory.Source = new BitmapImage(new Uri(path1 + "\\Logo\\HistoryIcon.png"));
+            iconAccount.Source = new BitmapImage(new Uri(path1 + "\\Logo\\AccountIcon.png"));
+            iconSignOut.Source = new BitmapImage(new Uri(path1 + "\\Logo\\SignOutIcon.png"));
+        }
+
+        private void btnFindWork_MouseEnter(object sender, MouseEventArgs e)
+        {
+            SolidColorBrush temp = (SolidColorBrush)new BrushConverter().ConvertFrom("#E4DCCF");
+            SolidColorBrush backgroundBrush = btnFindWork.Background as SolidColorBrush;
+            if (backgroundBrush != null && backgroundBrush.Color != temp.Color)
+            {
+                btnFindWork.Background = (Brush)new BrushConverter().ConvertFrom("#EFEFEF");
+            }
+        }
+
+        private void btnFindWork_MouseLeave(object sender, MouseEventArgs e)
+        {
+            SolidColorBrush temp = (SolidColorBrush)new BrushConverter().ConvertFrom("#E4DCCF");
+            SolidColorBrush backgroundBrush = btnFindWork.Background as SolidColorBrush;
+            if (backgroundBrush != null && backgroundBrush.Color != temp.Color)
+            { 
+                btnFindWork.Background = (Brush)new BrushConverter().ConvertFrom("#F9F5EB");
+            }
+        }
+
+        private void btnHistory_MouseEnter(object sender, MouseEventArgs e)
+        {
+            SolidColorBrush temp = (SolidColorBrush)new BrushConverter().ConvertFrom("#E4DCCF");
+            SolidColorBrush backgroundBrush = btnHistory.Background as SolidColorBrush;
+            if (backgroundBrush != null && backgroundBrush.Color != temp.Color)
+            {
+                btnHistory.Background = (Brush)new BrushConverter().ConvertFrom("#EFEFEF");
+            }
+        }
+
+        private void btnHistory_MouseLeave(object sender, MouseEventArgs e)
+        {
+            SolidColorBrush temp = (SolidColorBrush)new BrushConverter().ConvertFrom("#E4DCCF");
+            SolidColorBrush backgroundBrush = btnHistory.Background as SolidColorBrush;
+            if (backgroundBrush != null && backgroundBrush.Color != temp.Color)
+            {
+                btnHistory.Background = (Brush)new BrushConverter().ConvertFrom("#F9F5EB");
+            }
+        }
+
+        private void btnAccount_MouseEnter(object sender, MouseEventArgs e)
+        {
+            SolidColorBrush temp = (SolidColorBrush)new BrushConverter().ConvertFrom("#E4DCCF");
+            SolidColorBrush backgroundBrush = btnAccount.Background as SolidColorBrush;
+            if (backgroundBrush != null && backgroundBrush.Color != temp.Color)
+            {
+                btnAccount.Background = (Brush)new BrushConverter().ConvertFrom("#EFEFEF");
+            }
+        }
+
+        private void btnAccount_MouseLeave(object sender, MouseEventArgs e)
+        {
+            SolidColorBrush temp = (SolidColorBrush)new BrushConverter().ConvertFrom("#E4DCCF");
+            SolidColorBrush backgroundBrush = btnAccount.Background as SolidColorBrush;
+            if (backgroundBrush != null && backgroundBrush.Color != temp.Color)
+            {
+                btnAccount.Background = (Brush)new BrushConverter().ConvertFrom("#F9F5EB");
+            }
+        }
+
+        private void btnSignOut_MouseEnter(object sender, MouseEventArgs e)
+        {
+            SolidColorBrush temp = (SolidColorBrush)new BrushConverter().ConvertFrom("#E4DCCF");
+            SolidColorBrush backgroundBrush = btnSignOut.Background as SolidColorBrush;
+            if (backgroundBrush != null && backgroundBrush.Color != temp.Color)
+            {
+                btnSignOut.Background = (Brush)new BrushConverter().ConvertFrom("#EFEFEF");
+            }
+        }
+
+        private void btnSignOut_MouseLeave(object sender, MouseEventArgs e)
+        {
+            SolidColorBrush temp = (SolidColorBrush)new BrushConverter().ConvertFrom("#E4DCCF");
+            SolidColorBrush backgroundBrush = btnSignOut.Background as SolidColorBrush;
+            if (backgroundBrush != null && backgroundBrush.Color != temp.Color)
+            {
+                btnSignOut.Background = (Brush)new BrushConverter().ConvertFrom("#F9F5EB");
+            }
+        }
+
+        private void btnFindWork_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            btnFindWork.Background = (Brush)new BrushConverter().ConvertFrom("#E4DCCF");
+            btnHistory.Background = (Brush)new BrushConverter().ConvertFrom("#F9F5EB");
+            btnAccount.Background = (Brush)new BrushConverter().ConvertFrom("#F9F5EB");
+            //Thịnh
+        }
+
+        private void btnHistory_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            btnFindWork.Background = (Brush)new BrushConverter().ConvertFrom("#F9F5EB");
+            btnHistory.Background = (Brush)new BrushConverter().ConvertFrom("#E4DCCF");
+            btnAccount.Background = (Brush)new BrushConverter().ConvertFrom("#F9F5EB");
+            //Thịnh
+        }
+
+        private void btnAccount_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            btnFindWork.Background = (Brush)new BrushConverter().ConvertFrom("#F9F5EB");
+            btnHistory.Background = (Brush)new BrushConverter().ConvertFrom("#F9F5EB");
+            btnAccount.Background = (Brush)new BrushConverter().ConvertFrom("#E4DCCF");
+            //Thịnh
+        }
+
+        private void btnSignOut_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WLogin wLogin = new WLogin();
+            wLogin.Show();
+            this.Close();
         }
     }
 }
