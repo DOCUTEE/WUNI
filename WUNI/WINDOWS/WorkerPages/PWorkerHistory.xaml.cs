9afc883a2344dd4813b88bd7339a471c815fee06
Quoc Huy <quochuy.working@gmail.com>
2024-05-09 22:46:35 +0700

    cs History

--- WUNI/WINDOWS/WorkerPages/PWorkerHistory.xaml.cs
+++ WUNI/WINDOWS/WorkerPages/PWorkerHistory.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,27 +28,85 @@ namespace WUNI.WINDOWS.WorkerPages
         {
             InitializeComponent();
         }
+        //public PWorkerHistory(string WorkerID)
+        //{
+        //    InitializeComponent();
+        //    this.workerID = WorkerID;
+        //    WorkedDAO workedDAO = new WorkedDAO();
+        //    List<string> orderIDs = workedDAO.GetOrderIDList(this.workerID);
+        //    OrderDAO orderDAO = new OrderDAO();
+        //    List<Order>orders = orderDAO.GetOrdersFrom(orderIDs);
+        //    ufgOrders.Children.Clear();
+        //    MessageBox.Show(orders.Count.ToString());
+        //    foreach (Order order in orders)
+        //    {
+        //        UCWorkedOrder uCWorkedOrder = new UCWorkedOrder(order,this.workerID);
+        //        ufgOrders.Children.Add(uCWorkedOrder);
+        //    }        
+        //}
+
+        //private void btnAnalysis_Click(object sender, RoutedEventArgs e)
+        //{
+        //    WWorkerAnalysis wWorkerAnalysis = new WWorkerAnalysis(this.workerID);
+        //    wWorkerAnalysis.Show();
+        //}
+
+
+
         public PWorkerHistory(string WorkerID)
         {
             InitializeComponent();
             this.workerID = WorkerID;
+            LoadOrdersForSelectedDate(true);
+        }
+
+        private void LoadOrdersForSelectedDate(bool flag)
+        {
+            ufgOrders.Children.Clear();
             WorkedDAO workedDAO = new WorkedDAO();
             List<string> orderIDs = workedDAO.GetOrderIDList(this.workerID);
+
+            DateTime selectedDate = DateTime.Today;
+            if (dtpPick.SelectedDate != null)
+                selectedDate = dtpPick.SelectedDate.Value;
             OrderDAO orderDAO = new OrderDAO();
-            List<Order>orders = orderDAO.GetOrdersFrom(orderIDs);
-            ufgOrders.Children.Clear();
-            MessageBox.Show(orders.Count.ToString());
+            List<Order> orders = orderDAO.GetOrdersFrom(orderIDs);
             foreach (Order order in orders)
             {
-                UCWorkedOrder uCWorkedOrder = new UCWorkedOrder(order,this.workerID);
-                ufgOrders.Children.Add(uCWorkedOrder);
-            }        
+                if(flag)
+                {
+                    UCWorkedOrder uCWorkedOrder = new UCWorkedOrder(order, this.workerID);
+                    ufgOrders.Children.Add(uCWorkedOrder);
+                }    
+                else
+                    if (order.IssueDate.Date == selectedDate)
+                    {
+                        UCWorkedOrder uCWorkedOrder = new UCWorkedOrder(order, this.workerID);
+                        ufgOrders.Children.Add(uCWorkedOrder);
+                    }
+            }
         }
+
 
         private void btnAnalysis_Click(object sender, RoutedEventArgs e)
         {
             WWorkerAnalysis wWorkerAnalysis = new WWorkerAnalysis(this.workerID);
             wWorkerAnalysis.Show();
+        }
+
+        private void dtpPick_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            LoadOrdersForSelectedDate(false);
+        }
+
+        private void btnAllHistory_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            LoadOrdersForSelectedDate(true);
+        }
+
+        private void btpickTime_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
         }
     }
 }
